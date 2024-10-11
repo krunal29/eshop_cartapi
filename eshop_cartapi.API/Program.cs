f06cbd68f22c729e31f942740a9d06ba3a29d779
krunal.patel <krunal.patel@ifour.com>
2024-10-11 14:49:04 +0530

    fix build

--- eshop_cartapi.API/Program.cs
+++ eshop_cartapi.API/Program.cs
@@ -11,7 +11,6 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using eshop_cartapi.API.Filters;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
